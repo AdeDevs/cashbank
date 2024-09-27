9595a0456167f9383effa30f6a43e3d3b8516628
AdeDevs <adeyemiakinyemi01@gmail.com>
2024-09-27 02:03:37 +0100

    updt

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import logo from "./assets/logo.png"
+
 function App() {
   return (
     <div>
-      <h1>Za Woruldo</h1>
-      <p>i'm fucking worn out</p>
+      <header>
+        <nav>
+          <img src={logo} alt="logo" />
+          <ul>
+            <li>Solutions</li>
+            <li>Industry</li>
+            <li>Pricing</li>
+            <li>Resources</li>
+            <li>Company</li>
+          </ul>
+          <button>Get Started</button>
+        </nav>
+      </header>
     </div>
   )
 }
